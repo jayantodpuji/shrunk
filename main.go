0ef7b0f4d1b43d3bd6ddad269af1df7214d4d7a0
jayantodpuji <dedypuji@outlook.com>
2024-11-11 23:37:33 +0700

    feat: simplify the app to only use api

--- main.go
+++ main.go
@@ -1,26 +1,59 @@
 package main
 
 import (
-	"html/template"
+	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
-func indexHandler(w http.ResponseWriter, _ *http.Request) {
-	indexTemplate, err := template.ParseFiles("static/index.html")
-	if err != nil {
-		http.Error(w, "unable to load template", http.StatusInternalServerError)
-		return
-	}
-
-	indexTemplate.Execute(w, nil)
-}
-
 func main() {
-	http.HandleFunc("/", indexHandler)
-
-	err := http.ListenAndServe(":3002", nil)
+	var err error
+	connStr := "user=postgres dbname=shrunk host=localhost port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal("server failed to start:", err)
+		log.Fatal("database connection failed", err)
 	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("error closing the database connection: %v", err)
+		}
+	}()
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("unable to reach the database: %v", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		type requestBody struct {
+			URL string `json:"url"`
+		}
+
+		var req requestBody
+		decoder := json.NewDecoder(r.Body)
+		if err = decoder.Decode(&req); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+	})
+
+	mux.HandleFunc("/:slug", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+	})
+
+	server := &http.Server{Addr: ":3002", Handler: mux}
+	server.ListenAndServe()
 }
